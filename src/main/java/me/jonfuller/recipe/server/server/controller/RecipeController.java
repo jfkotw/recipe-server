ffa0dbf1a4dfe134dfeb6b1f92e2cc66465630c0
Jon Fuller <2632210+jfkotw@users.noreply.github.com>
2020-04-14 22:22:55 +0200

    Update the example.json Add s3 dependency. Update recipe controller to perform redirects to S3 bucket: https://blog.ascendingdc.com/quick-tutorial-to-integrate-amazon-s3-presigned-url/ has a good list of pros / cons of passthrough, vs. S3 URL, vs. pre-signed URL. Update OpenAPI spec to provide images.

--- src/main/java/me/jonfuller/recipe/server/server/controller/RecipeController.java
+++ src/main/java/me/jonfuller/recipe/server/server/controller/RecipeController.java
@@ -4,17 +4,21 @@ import me.jonfuller.recipe.api.RecipesApi;
 import me.jonfuller.recipe.api.model.Ingredient;
 import me.jonfuller.recipe.api.model.Method;
 import me.jonfuller.recipe.api.model.Recipe;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.*;
 import software.amazon.awssdk.services.dynamodb.paginators.ScanIterable;
 import javax.validation.Valid;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 
 @RestController
@@ -26,6 +30,8 @@ public class RecipeController implements RecipesApi {
     private static final Region region = Region.EU_WEST_1;
     private static final String profileName = "default";
     private static final String tableName = "recipes";
+    private static final String imageBucket = "recipes.jonfuller.me";
+    private static final String imageBucketHost = "s3-eu-west-1.amazonaws.com";
 
     private static DynamoDbClient client = DynamoDbClient.builder()
             .region(region)
@@ -116,11 +122,11 @@ public class RecipeController implements RecipesApi {
 //        List<AttributeValue> tagList = new ArrayList<>();
 //
 //        for (String tag : recipe.getTags()) {
-//            tagList.add(AttributeValue.builde("tags", tag).build());
+//            tagList.add(AttributeValue.builder("tags", tag).build());
 //        }
 //        item.put("tags", AttributeValue.builder().l(AttributeValue.builder().l(tagMap).build()).build());
 
-        item.put("image", AttributeValue.builder().s(recipe.getImage()).build());
+        item.put("image", AttributeValue.builder().s(recipe.getImageKey()).build());
 
         return item;
     }
@@ -161,7 +167,7 @@ public class RecipeController implements RecipesApi {
         }
         recipe.setTags(tagList);
 
-        recipe.setImage(item.get("image").s());
+        recipe.setImageKey(item.get("image").s());
 
         return recipe;
     }
@@ -180,7 +186,7 @@ public class RecipeController implements RecipesApi {
 
         try {
             Map<String,AttributeValue> returnedItem = client.getItem(req).item();
-
+            //return ResponseStatus(HttpStatus.MOVED_PERMANENTLY, "https://s3.eu-west-1.amazonaws.com/recipes.jonfuller.me/recipe-api.yml?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200413T202629Z&X-Amz-SignedHeaders=host&X-Amz-Expires=600&X-Amz-Credential=AKIAQFGBK5E5QSC47DV3%2F20200413%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Signature=22482dd98518d4c66e4e5c1bc97a8de5e615f4e83b4719a85c382f4d2c3c13c2");
             if(returnedItem != null) {
                 return ResponseEntity.ok(queryToRecipe(returnedItem));
 
@@ -194,5 +200,40 @@ public class RecipeController implements RecipesApi {
                     HttpStatus.INTERNAL_SERVER_ERROR, "Ooops"
             );
         }
+    }
+
+    @Override
+    public ResponseEntity<Void> showRecipeImage(String recipeId) {
+        HashMap<String,AttributeValue> keyToGet = new HashMap<>();
+
+        keyToGet.put("id", AttributeValue.builder()
+                .n(recipeId).build());
+
+        GetItemRequest req = GetItemRequest.builder()
+                .key(keyToGet)
+                .tableName("recipes")
+                .build();
+
+        try {
+            Map<String, AttributeValue> returnedItem = client.getItem(req).item();
+            if(returnedItem != null) {
+                Recipe recipe = queryToRecipe(returnedItem);
+                UriComponents imageFileLocation = UriComponentsBuilder.newInstance()
+                        .scheme("https").host(imageBucketHost).pathSegment(imageBucket, recipe.getImageKey()).build();
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.add("Location", imageFileLocation.toUriString());
+                return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+            } else {
+                throw new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "No image for recipe with ID: "+recipeId
+                );
+            }
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR, "Ooops"
+            );
+        }
+
     }
 }
